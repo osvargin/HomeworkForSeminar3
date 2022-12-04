8119abe323a5ae09491cd7d83ec4a4990cdd30df
osvargin <oleg_197@bk.ru>
2022-12-04 20:07:48 +0300

    Add Task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+
+Console.WriteLine("Введите пятизначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 10000 && number < 100000)
+    {
+    int division1 = (number / 10000);
+    int remainder1 = (number % 10);
+    int division2 = (number / 1000) % 10;
+    int remainder2 = (number / 10 % 10);
+    if (division1 == remainder1 && division2 == remainder2) Console.WriteLine("Да");
+    else Console.WriteLine("Нет");
+    }
+else 
+    {
+    Console.WriteLine("Некорректное число!");
+    }

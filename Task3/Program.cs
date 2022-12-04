5032a62eaa678324b86887bc3b4dfb4c713c2f44
osvargin <oleg_197@bk.ru>
2022-12-04 20:53:34 +0300

    add Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход 2 числа (N,M) и выдаёт таблицу кубов чисел от N до M.
+// 2,3 -> 8, 27
+// 1,5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+Console.WriteLine("Введите число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число M:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int count = n;
+
+while (count >= n && count <= m)
+{
+    Console.Write($"{count * count * count}, ");
+    count++;
+}
